0a66d6615f38936e48c8614dd46cfacc00d7ca5a
vikash-connect <vikash.connect05@gmail.com>
2025-12-10 23:51:29 +0530

    Add files via upload

---
+++ half_the_number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int num;
+    scanf("%d", &num);
+    float a= num/2.00;
+    printf("Half of %d is %.2f", num, a);
+    
+    
+    return 0;
+}
