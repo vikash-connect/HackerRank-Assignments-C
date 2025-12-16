84bc5deeffa31a408c7079d51aac8a4f2aff286f
vikash-connect <vikash.connect05@gmail.com>
2025-12-16 23:40:38 +0530

    Add files via upload

---
+++ data_types.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+scanf("%d %ld %c %f %lf",&a,&b,&c,&d,&e);
+
+printf("%d\n%ld\n%c\n%.3f\n%.9lf",a,b,c,d,e);
+    return 0;
+}
