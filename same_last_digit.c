be5af8164d3fb8171698402462208eb5783b638b
vikash-connect <vikash.connect05@gmail.com>
2025-12-19 00:06:10 +0530

    same_last_digit.c

--- same_last_digit.c
+++ same_last_digit.c
@@ -5,19 +5,13 @@
 
 int main() {
 
-int a,b;
-scanf("%d %d",&a,&b);
-
-a=a%10;
-b=b%10;
-
-if(a==b)
-{
-printf("YES");
-
-
-}
-else{printf("NO");}
-
+    int a;
+    int b;
+    scanf("%d %d", &a, &b);
+    if(a%10==b%10)
+        printf("YES");
+    else
+        printf("NO");
+    
 return 0;
 }
