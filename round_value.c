f324dc5f2e6f89bbc237b4e395376ce35a877667
Vikash Kr. <vikash.connect05@gmail.com>
2025-12-23 22:57:01 +0530

    Add files via upload

---
+++ round_value.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    float num;
+    scanf("%f",&num);
+
+   int num1= (int)num;
+    printf("Rounded Value = %d",num1);
+    return 0;
+}
